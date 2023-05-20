2cbbe557f858ebef88ed3abaa51392d9175fd293
TinyNick <nick@blender.org>
2023-05-20 02:08:39 -0400

    Create Modal Motion Capture Operator for Controllers

--- __init__.py
+++ __init__.py
@@ -25,8 +25,11 @@ class VIEW3D_PT_vr_save_position(bpy.types.Panel):
     bl_label = "VR Capture"
     
     def draw(self, context):
-        self.layout.prop(context.scene, "vr_target")
-        self.layout.operator("view3d.vr_save_pose")
+        row = self.layout.row()
+        row.prop(context.scene, "vr_target_left", text= "Left")
+        row.prop(context.scene, "vr_target_right", text="Right")
+        enabled = context.scene.vr_motion_capture
+        self.layout.operator("view3d.vr_save_pose", depress=enabled)
 
 
 class VIEW3D_OT_vr_save_position(bpy.types.Operator):
@@ -34,40 +37,62 @@ class VIEW3D_OT_vr_save_position(bpy.types.Operator):
     bl_label = "Save VR Poses"
 
     ## TODO WARN USER IF VR ADDON IS NOT ENABLED OR SESSIONS IS NOT STARTED
+    _left_target = None
+    _right_target = None
+    _wm = None
 
     @staticmethod
-    def _get_controller_pose_matrix(context, idx, is_grip, scale):
-        wm = context.window_manager
-
-        loc = None
-        rot = None
-        if is_grip:
-            loc = wm.xr_session_state.controller_grip_location_get(context, idx)
-            rot = wm.xr_session_state.controller_grip_rotation_get(context, idx)
-        else:
-            loc = wm.xr_session_state.controller_aim_location_get(context, idx)
-            rot = wm.xr_session_state.controller_aim_rotation_get(context, idx)
-
+    def _get_controller_pose_matrix(context, idx, wm):
+        scale=1.0
+        loc = wm.xr_session_state.controller_grip_location_get(context, idx)
+        rot = wm.xr_session_state.controller_grip_rotation_get(context, idx)
         rotmat = Matrix.Identity(3)
         rotmat.rotate(Quaternion(Vector(rot)))
         rotmat.resize_4x4()
         transmat = Matrix.Translation(loc)
         scalemat = Matrix.Scale(scale, 4)
         return transmat @ rotmat @ scalemat
+    
+    def keyframe_grip(self, context, idx:int,wm):
+        target_object = self._left_target if idx == 0 else self._right_target
+        target_object.matrix_basis = self._get_controller_pose_matrix(context, idx, wm)
+        target_object.keyframe_insert('location')
+        target_object.keyframe_insert('rotation_euler')
+
+    def create_new_action(self,  obj:bpy.types.Object):
+        obj.animation_data_create()
+        action = bpy.data.actions.new(obj.name)
+        obj.animation_data.action = action
+        return obj.animation_data.action
+
+    def modal(self, context, event):
+        if event.type in {'RIGHTMOUSE', 'ESC'}:
+            context.scene.vr_motion_capture = False
+            return {'CANCELLED'}
+        
+        if context.scene.frame_current == context.scene.frame_end:
+            context.scene.vr_motion_capture = False
+            return {'FINISHED'}
+        self.keyframe_grip(context, 0, self._wm)
+        self.keyframe_grip(context, 1, self._wm)
+        return {'PASS_THROUGH'}
 
     def execute(self, context):
-        if context.scene.vr_target is None:
+        if context.scene.vr_target_left is None or  context.scene.vr_target_right is None:
             self.report({'ERROR'}, "No object is set")
-            return {'CANCELLED'} 
-        idx = 0
-        is_grip = True
+            return {'CANCELLED'}
+        context.scene.vr_motion_capture = True
+        self._left_target = context.scene.vr_target_left
+        self._right_target = context.scene.vr_target_right
+        self.create_new_action(self._left_target)
+        self.create_new_action(self._right_target)
+        self._left_target
+        self._wm = context.window_manager
+        self._wm.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+    
 
-        obj = context.scene.vr_target 
-        obj.matrix_basis = self._get_controller_pose_matrix(context, idx, is_grip, 1.0)
-        obj.keyframe_insert('location')
-        obj.keyframe_insert('rotation_euler')
 
-        return {'FINISHED'}
 
 
 
@@ -75,9 +100,12 @@ classes = (VIEW3D_OT_vr_save_position,VIEW3D_PT_vr_save_position)
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    bpy.types.Scene.vr_target = bpy.props.PointerProperty(type=bpy.types.Object)
+    bpy.types.Scene.vr_target_left = bpy.props.PointerProperty(type=bpy.types.Object)
+    bpy.types.Scene.vr_target_right = bpy.props.PointerProperty(type=bpy.types.Object)
+    bpy.types.Scene.vr_motion_capture = bpy.props.BoolProperty(name="VR Motion Capture Enabled", default=False)
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-    del bpy.types.Scene.vr_target
+    del bpy.types.Scene.vr_target_left
+    del bpy.types.Scene.vr_target_right
