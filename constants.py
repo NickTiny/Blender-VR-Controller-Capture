aeb132dbd752b8b0504b672ab124b662c4029254
Nick Alberelli <nick@tinymedia.ca>
2024-05-09 17:53:18 -0400

    Re-Use VRDefaultActions in constants

--- constants.py
+++ constants.py
@@ -1,11 +1,13 @@
+from .mocap_actionmaps import VRDefaultActions
+
 BONE_ROTATION_MODE = 'XYZ'
 
 CONTROLLER_BUTTONS = [
-    ("fly_forward", "Up/Down Stick", "Thumbstick up and down direction. Up is positive Down is negative, not captured on Left Controller" ),
-    ("fly_left", "Left/Right Stick", "Thumbstick left and right direction. Left is positive, Right is negative only captured on Left Controller"),
-    ("nav_reset", "A Button", "A Button on Quest or equivalant 'nav_reset' button for your device"),
-    ("nav_grab", "Inner Trigger", "Inner Trigger on Questor equivalant 'nav_grab' button for your device"),
-    ("teleport", "Outer Trigger", "Outer Trigger on Quest or equivalant 'teleport' button for your device")
+    (VRDefaultActions.FLY_FORWARD, "Up/Down Stick", "Thumbstick up and down direction. Up is positive Down is negative, not captured on Left Controller" ),
+    (VRDefaultActions.FLY_LEFT, "Left/Right Stick", "Thumbstick left and right direction. Left is positive, Right is negative only captured on Left Controller"),
+    (VRDefaultActions.NAV_RESET, "A Button", "A Button on Quest or equivalant 'nav_reset' button for your device"),
+    (VRDefaultActions.NAV_GRAB, "Inner Trigger", "Inner Trigger on Questor equivalant 'nav_grab' button for your device"),
+    (VRDefaultActions.TELEPORT, "Outer Trigger", "Outer Trigger on Quest or equivalant 'teleport' button for your device")
 
 ]
 ALL_CONTROLLER_BUTTON_PROPS = [prop[1] for prop in CONTROLLER_BUTTONS]
